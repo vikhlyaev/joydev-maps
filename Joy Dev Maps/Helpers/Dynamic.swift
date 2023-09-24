c604d899b745f3fe11c59448f9022091acd7834e
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-09-24 10:32:04 +0500

    adds dynamic wrapper for mvvm implementation

---
+++ Joy Dev Maps/Helpers/Dynamic.swift
@@ -0,0 +1,29 @@
+//
+//  Dynamic.swift
+//  Joy Dev Maps
+//
+//  Created by Anton Vikhlyaev on 22.09.2023.
+//
+
+import Foundation
+
+final class Dynamic<T> {
+    typealias Listener = (T) -> Void
+    
+    private var listener: Listener?
+    
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(_ listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+}
