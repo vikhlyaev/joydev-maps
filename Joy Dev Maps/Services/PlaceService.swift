788d83aeeb02d27aef7b3232dc66ef6961147b4a
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-09-25 16:46:50 +0500

    adds place service

---
+++ Joy Dev Maps/Services/PlaceService.swift
@@ -0,0 +1,83 @@
+//
+//  PlaceService.swift
+//  Joy Dev Maps
+//
+//  Created by Anton Vikhlyaev on 24.09.2023.
+//
+
+import Foundation
+
+protocol PlaceService {
+    func getPlaces(by query: String, and coordinate: LocationCoordinate, _ completion: @escaping (Result<[Place], Error>) -> Void)
+}
+
+// MARK: - Implementation
+
+final class PlaceServiceImpl: PlaceService {
+    
+    // MARK: Properties
+    
+    private let networkService: NetworkService
+    
+    // MARK: Life Cycle
+    
+    init(networkService: NetworkService) {
+        self.networkService = networkService
+    }
+    
+    // MARK: Private functions
+    
+    private func prepareRequest(with query: String, and coordinate: LocationCoordinate) -> URLRequest? {
+        guard
+            let host = Bundle.main.object(forInfoDictionaryKey: "URL Host") as? String,
+            let path = Bundle.main.object(forInfoDictionaryKey: "URL Path") as? String,
+            let apiKey = Bundle.main.object(forInfoDictionaryKey: "API Key") as? String
+        else { return nil }
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = host
+        urlComponents.path = path
+        urlComponents.queryItems = [
+            URLQueryItem(name: "query", value: query),
+            URLQueryItem(name: "ll", value: "\(coordinate.latitude),\(coordinate.longitude)"),
+            URLQueryItem(name: "open_now", value: "true"),
+            URLQueryItem(name: "sort", value: "DISTANCE"),
+            URLQueryItem(name: "radius", value: "100000")
+        ]
+        guard let url = urlComponents.url else { return nil }
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue(apiKey, forHTTPHeaderField: "Authorization")
+        return request
+    }
+    
+    // MARK: PlaceService
+    
+    func getPlaces(by query: String, and coordinate: LocationCoordinate, _ completion: @escaping (Result<[Place], Error>) -> Void) {
+        let completionOnMainQueue: (Result<[Place], Error>) -> Void = { result in
+            DispatchQueue.main.async {
+                completion(result)
+            }
+        }
+        guard let request = prepareRequest(with: query, and: coordinate) else { return }
+        networkService.fetch(for: request) { results in
+            switch results {
+            case .success(let data):
+                do {
+                    let places = try JSONDecoder().decode(Places.self, from: data)
+                    completionOnMainQueue(.success(places.results))
+                } catch let error {
+                    completionOnMainQueue(.failure(PlaceServiceError.parsingJsonError(error)))
+                }
+            case .failure(let error):
+                completionOnMainQueue(.failure(error))
+            }
+        }
+    }
+}
+
+// MARK: - Place Service Error
+
+enum PlaceServiceError: Error {
+    case parsingJsonError(Error)
+}
