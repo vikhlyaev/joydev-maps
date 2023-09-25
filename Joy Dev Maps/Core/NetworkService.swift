82374635f24db18c8922c3926fd4879229f084a2
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-09-25 16:25:32 +0500

    adds network service

--- Joy Dev Maps/Core/NetworkService.swift
+++ Joy Dev Maps/Core/NetworkService.swift
@@ -8,11 +8,40 @@
 import Foundation
 
 protocol NetworkService {
-    
+    func fetch(for request: URLRequest, _ completion: @escaping (Result<Data, Error>) -> Void)
 }
 
 // MARK: - Implementation
 
 final class NetworkServiceImpl: NetworkService {
     
+    // MARK: Properties
+    
+    private let session = URLSession.shared
+    
+    // MARK: NetworkService
+    
+    func fetch(for request: URLRequest, _ completion: @escaping (Result<Data, Error>) -> Void) {
+        session.dataTask(with: request, completionHandler: { data, response, error in
+            if let data, let response, let statusCode = (response as? HTTPURLResponse)?.statusCode {
+                if 200 ..< 300 ~= statusCode {
+                    completion(.success(data))
+                } else {
+                    completion(.failure(NetworkError.httpStatusCode(statusCode)))
+                }
+            } else if let error {
+                completion(.failure(NetworkError.urlRequestError(error)))
+            } else {
+                completion(.failure(NetworkError.urlSessionError))
+            }
+        }).resume()
+    }
+}
+
+// MARK: - Network Error
+
+enum NetworkError: Error {
+    case httpStatusCode(Int)
+    case urlRequestError(Error)
+    case urlSessionError
 }
