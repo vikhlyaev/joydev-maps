3613aa8105244078b190b40c5bea9afcd25fbf7c
Anton Vikhlyaev <vikhlyaev@vikhlyaev.ru>
2023-09-28 09:03:44 +0500

    adds user model

---
+++ Joy Dev Maps/Models/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Joy Dev Maps
+//
+//  Created by Anton Vikhlyaev on 28.09.2023.
+//
+
+import Foundation
+
+struct User: Codable {
+    let login: String
+    let email: String
+    let password: String
+}
